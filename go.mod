a649c5b6e2a6d311821b1497338849cb1174b38c
keribend <keribend@gmail.com>
2023-12-03 18:03:05 +0100

    WIP

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module sandbox/hello-web
 
 go 1.20
+
+require go.uber.org/zap v1.26.0
+
+require (
+	github.com/go-chi/chi/v5 v5.0.10
+	go.uber.org/multierr v1.10.0 // indirect
+)
--- ui/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,7 +0,0 @@
-
-
-export const index = 1;
-export const component = async () => (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/entry/error.svelte.7d5946a1.js","_app/immutable/chunks/index.ab0e7057.js","_app/immutable/chunks/singletons.dc45313c.js"];
-export const stylesheets = [];
-export const fonts = [];
