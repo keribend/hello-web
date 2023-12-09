6b8d23d9f46377cd1bf433722970ae9298b1f139
Neil <kerib@pop-os.localdomain>
2023-12-09 01:13:37 +0100

    WIP: first draft of a working HTMX basic app

--- cmd/web/webengine.go
+++ cmd/web/webengine.go
@@ -1,15 +1,23 @@
 package main
 
 import (
-	// "fmt"
 	"html/template"
 	"net/http"
 
 	"go.uber.org/zap"
 )
 
+type Counter struct {
+	ID    int
+	Name  string
+	Value uint
+}
+
+var counters = []Counter{}
+
 type WebEngine struct {
-	log *zap.Logger
+	log       *zap.Logger
+	templates *template.Template
 }
 
 func NewWebEngine() *WebEngine {
@@ -19,11 +27,36 @@ func NewWebEngine() *WebEngine {
 		panic(err)
 	}
 	e.log = log
+	e.templates, err = e.ParseTemplates()
+	if err != nil {
+		e.log.Error("ParseTemplates", zap.Error(err))
+	}
 	return &e
+}
+
+func (e *WebEngine) ParseTemplates() (*template.Template, error) {
+	return template.ParseGlob("cmd/web/ui/*")
+
 }
 
 func (e *WebEngine) Serve(w http.ResponseWriter, r *http.Request) {
 	e.log.Debug("Serve")
-	tmpl := template.Must(template.ParseFiles("cmd/web/ui/index.html"))
-	tmpl.Execute(w, nil)
+	e.templates.ExecuteTemplate(w, "homeHTML", counters)
+}
+
+func (e *WebEngine) GetCounters(w http.ResponseWriter, r *http.Request) {
+	e.log.Debug("GetCounters")
+	e.templates.ExecuteTemplate(w, "countersHTML", counters)
+}
+
+func (e *WebEngine) CreateCounter(w http.ResponseWriter, r *http.Request) {
+	e.log.Debug("CreateCounter")
+}
+
+func (e *WebEngine) IncreaseCounter(w http.ResponseWriter, r *http.Request) {
+	e.log.Debug("IncreaseCounter")
+}
+
+func (e *WebEngine) DecreaseCounter(w http.ResponseWriter, r *http.Request) {
+	e.log.Debug("DecreaseCounter")
 }
